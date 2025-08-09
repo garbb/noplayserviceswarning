09bedf9637b48146eb45cc93330ebf83aef1850d
garbb <e456h@yahoo.com>
2025-08-09 14:55:08 -0700

    only need to spoof getPageInfo

--- app/src/main/java/com/noplayserviceswarning/MainHook.java
+++ app/src/main/java/com/noplayserviceswarning/MainHook.java
@@ -1,20 +1,24 @@
 package com.noplayserviceswarning;
 
-import android.app.Application;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import java.lang.reflect.Field;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
+
+    private void setField(Object Obj, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
+        Field field = Obj.getClass().getField(fieldName); // Only finds public fields
+        field.setAccessible(true); // Allow access if it's not public
+        field.set(Obj, value);
+    }
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
@@ -25,7 +29,7 @@ public class MainHook implements IXposedHookLoadPackage {
                 "getPackageInfo",
                 new XC_MethodHook() {
                     @Override
-                    protected void afterHookedMethod(MethodHookParam param) {
+                    protected void afterHookedMethod(MethodHookParam param) throws NoSuchFieldException, IllegalAccessException {
                         if (param.args.length >= 1 && param.args[0] instanceof String) {
 
                             final String GMS_PACKAGE_NAME = "com.google.android.gms";
@@ -38,7 +42,21 @@ public class MainHook implements IXposedHookLoadPackage {
 
                                 // empty ApplicationInfo makes app skip google play services check for some reason...
                                 // also causes some apps to not even create google play services availability notification channel...
-                                fakeInfo.applicationInfo = new ApplicationInfo();
+                                //fakeInfo.applicationInfo = new ApplicationInfo();
+
+                                int fakeVersionCode = Integer.MAX_VALUE; // 2147483647
+//                                int fakeVersionCode = 252863013;
+//                                int fakeVersionCode = 12862063L;
+
+                                fakeInfo.setLongVersionCode(fakeVersionCode);
+
+                                ApplicationInfo appInfo = fakeInfo.applicationInfo;
+                                appInfo.enabled = true;
+                                setField(appInfo, "enabledSetting", PackageManager.COMPONENT_ENABLED_STATE_ENABLED);
+                                setField(appInfo, "longVersionCode", fakeVersionCode);
+                                setField(appInfo, "versionCode", fakeVersionCode);
+
+                                PackageInfoLogger.logPackageInfo(fakeInfo);
 
                                 param.setResult(fakeInfo);
                             }
@@ -48,72 +66,6 @@ public class MainHook implements IXposedHookLoadPackage {
         );
 
 
-
-        String targetClass = "com.google.android.gms.common.GoogleApiAvailabilityLight";
-//        String targetClass = "com.google.android.gms.common.GoogleApiAvailability";
-//        String targetClass = "com.google.android.gms.common.GooglePlayServicesUtilLight";
-        Class<?> classexists = XposedHelpers.findClassIfExists(targetClass, lpparam.classLoader);
-        if (classexists != null) {
-            try {
-                XposedBridge.hookAllMethods(
-                        classexists, // className
-                        "isGooglePlayServicesAvailable", // the method to hook
-                        new XC_MethodReplacement() {
-                            @Override
-                            protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) {
-                                XposedBridge.log("making isGooglePlayServicesAvailable return 0 for " + lpparam.packageName);
-                                // isGooglePlayServicesAvailable returns 0 if there are no errors
-                                // This way the method it's not executed and 0 is always returned
-                                return 0;
-                            }
-                        }
-                );
-                XposedBridge.log("hooked isGooglePlayServicesAvailable for : " + lpparam.packageName);
-            }
-            catch (NoSuchMethodError e) {
-                XposedBridge.log("Error: \"" + lpparam.appInfo.name + "\" does not contain the target method..\n");
-            }
-            catch (Exception e) {
-                XposedBridge.log("Unexpected error :\n" + e.getMessage());
-            }
-
-        }
-
-
-        // when notification channel is created, set importance to NONE
-        // this happens upon first launch after data clear
-        XposedHelpers.findAndHookMethod(NotificationManager.class, "createNotificationChannel",
-            NotificationChannel.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) {
-                        NotificationChannel channel = (NotificationChannel) param.args[0];
-                        if (channel.getId().equals("com.google.android.gms.availability")) {
-                            XposedBridge.log("hooked createNotificationChannel for " + channel.getId());
-                            channel.setImportance(NotificationManager.IMPORTANCE_NONE);
-                        }
-                    }
-                });
-
-        // if channel already exists then change importance to NONE
-        XposedHelpers.findAndHookMethod(Application.class, "attach",
-                Context.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) {
-                        Context context = (Context) param.args[0];
-
-                        NotificationManager nm = context.getSystemService(NotificationManager.class);
-                        NotificationChannel channel = nm.getNotificationChannel("com.google.android.gms.availability");
-
-                        if (channel != null) {
-                            XposedBridge.log("setting importance for " + channel.getId() + " to IMPORTANCE_NONE");
-                            channel.setImportance(NotificationManager.IMPORTANCE_NONE);
-                            nm.createNotificationChannel(channel);
-                        }
-
-                    }
-                });
 
 
     }
