5686943030356ed1362adbda8a603521bdfadce3
garbb <e456h@yahoo.com>
2025-08-10 10:45:48 -0700

    don't need bouncycastle in apk

--- app/src/main/java/com/noplayserviceswarning/MainHook.java
+++ app/src/main/java/com/noplayserviceswarning/MainHook.java
@@ -8,14 +8,13 @@ import java.lang.reflect.Field;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
 
-    private void setField(Object targetObject, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
+    private void setField(Object targetObject, String fieldName, Object value) {
         if (targetObject == null) {
             return;
         }
@@ -43,7 +42,7 @@ public class MainHook implements IXposedHookLoadPackage {
                 "getPackageInfo",
                 new XC_MethodHook() {
                     @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws NoSuchFieldException, IllegalAccessException {
+                    protected void afterHookedMethod(MethodHookParam param) {
                         if (param.args.length >= 1 && param.args[0] instanceof String) {
 
                             final String GMS_PACKAGE_NAME = "com.google.android.gms";
@@ -52,24 +51,24 @@ public class MainHook implements IXposedHookLoadPackage {
 
                             if (GMS_PACKAGE_NAME.equals(packageName)) {
                                 XposedBridge.log("Spoofing getPackageInfo() for: " + packageName);
-                                PackageInfo fakeInfo = (PackageInfo) param.getResult();
+                                PackageInfo packageInfo = (PackageInfo) param.getResult();
 
                                 int fakeVersionCode = Integer.MAX_VALUE; // 2147483647
 //                                int fakeVersionCode = 252863013;
 //                                int fakeVersionCode = 12862063L;
 
-                                fakeInfo.setLongVersionCode(fakeVersionCode);
-//
-                                ApplicationInfo appInfo = fakeInfo.applicationInfo;
+                                packageInfo.setLongVersionCode(fakeVersionCode);
+
+                                ApplicationInfo appInfo = packageInfo.applicationInfo;
                                 appInfo.enabled = true;
                                 setField(appInfo, "enabledSetting", PackageManager.COMPONENT_ENABLED_STATE_ENABLED);
                                 setField(appInfo, "longVersionCode", fakeVersionCode);
                                 setField(appInfo, "versionCode", fakeVersionCode);
 
                                 // for debugging...
-                                //PackageInfoLogger.logPackageInfo(fakeInfo);
+                                //PackageInfoLogger.logPackageInfo(packageInfo);
 
-                                //param.setResult(fakeInfo);
+                                //param.setResult(packageInfo);
                             }
                         }
                     }
